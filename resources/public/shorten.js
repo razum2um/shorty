aa5b648dc146838f50f9907586831a7ea3565d32
Vlad Bokov <razum2um@mail.ru>
2015-02-22 16:17:37 +0600

    add GA

--- resources/public/shorten.js
+++ resources/public/shorten.js
@@ -14,3 +14,11 @@ $(document).ready(function() {
     return false;
   });
 });
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-60004875-1', 'auto');
+ga('send', 'pageview');
